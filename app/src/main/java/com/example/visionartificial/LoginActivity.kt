38f71e0a0e375ed5de1f72626d65c20d9c3e18ca
Alexis Solis <alexissolis687@gmail.com>
2024-11-19 16:57:55 -0600

    1..- Se desarrollo la interfaz del registro de usuarios. 2.- Se hizo la validacion de datos para el registro de usuarios con Firebase. 3.- Se diseño y programo un boton de cerrar sesion en el xml y kt del mainactivity 4.- Se programo la actividad login para iniciar sesion. 5.- Se rediseño el registro y login con una interfaz mas intuitiva.

--- app/src/main/java/com/example/visionartificial/LoginActivity.kt
+++ app/src/main/java/com/example/visionartificial/LoginActivity.kt
@@ -1,20 +1,68 @@
 package com.example.visionartificial
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var L_Et_email :EditText
+    private lateinit var L_Et_password : EditText
+    private lateinit var Btn_login : Button
+    private lateinit var auth : FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        InicializarVariables()
+
+        Btn_login.setOnClickListener {
+            validarDatos()
         }
+    }
+
+
+    private fun InicializarVariables(){
+        L_Et_email = findViewById(R.id.L_Et_email)
+        L_Et_password = findViewById(R.id.L_Et_password)
+        Btn_login = findViewById(R.id.Btn_login)
+        auth = FirebaseAuth.getInstance()
+    }
+
+    private fun validarDatos() {
+        val email : String = L_Et_email.text.toString()
+        val password : String = L_Et_password.text.toString()
+
+        if (email.isEmpty()){
+            Toast.makeText(applicationContext, "Ingrese su correo electronico", Toast.LENGTH_SHORT).show()
+        }
+        if (password.isEmpty()){
+            Toast.makeText(applicationContext,"Ingrese su contraseña", Toast.LENGTH_SHORT).show()
+        }else{
+            LoginUsuario(email, password)
+        }
+    }
+
+    private fun LoginUsuario(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task->
+            if (task.isSuccessful){
+                val intent = Intent(this@LoginActivity, MainActivity::class.java)
+                Toast.makeText(applicationContext,"Iniciando Sesion...", Toast.LENGTH_SHORT).show()
+                startActivity(intent)
+                finish()
+            }else{
+                Toast.makeText(applicationContext,"Ha ocurrido un error", Toast.LENGTH_SHORT).show()
+            }
+        }.addOnFailureListener { e->
+                Toast.makeText(applicationContext,"{${e.message}}", Toast.LENGTH_SHORT).show()
+            }
     }
 }
